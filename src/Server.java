113487ad2acbf0d559865b9cebd27dbdab41547c
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-02-21 00:57:53 +0600

    refactor: Moved server creation from Main to Server class

---
+++ src/Server.java
@@ -0,0 +1,32 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Server {
+    private final HttpServer httpServer;
+    private final RequestHandler requestHandler;
+    private final InetSocketAddress addr;
+
+    public Server(String host, int port, int backlog) throws IOException {
+        this.requestHandler = new RequestHandler();
+        this.addr = new InetSocketAddress(host, port);
+        this.httpServer = HttpServer.create(addr, backlog);
+        initRoutes();
+    }
+
+    public void start() {
+        httpServer.start();
+        System.out.printf(
+                "Сервер был запущен на 'http://%s:%d'\n",
+                addr.getHostName(),
+                addr.getPort()
+        );
+    }
+
+    private void initRoutes() {
+        httpServer.createContext("/", requestHandler::handleMainRequest);
+        httpServer.createContext("/apps", requestHandler::handleAppsRequest);
+        httpServer.createContext("/apps/profile", requestHandler::handleProfileRequest);
+    }
+}
